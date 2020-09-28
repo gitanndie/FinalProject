d6e15ba731c65cf0de080337d21c02feea45e987
jhonatanquirama <quiramapino47@gmail.com>
2020-09-28 18:03:05 -0500

    actualizaciones

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -15,144 +15,144 @@ export default class Review extends Component {
           <h4>En esta Evaluación mediremos tu conocimientos</h4>
           <h6>Esfuerzate y así ganarás muchos puntos</h6>
           <img src={cabezarobot} id="cabezarobot" alt="robot" />
-          <section>
-            <p>
-              1.  ¿Qué propiedad utilizamos para definir una imagen como fondo?
-            </p>
-            <div className="control">
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                background-attachment
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                background-image
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                background-src
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                background-img
-              </label>
-              <br />
-            </div>
-            {/* <img src={grafica} alt="grafica"/> */}
-          </section>
-          <section>
-            <p>
-              2.   ¿Cuál es la forma más eficiente de aplicar estilos CSS en un
-              documento HTML?
-            </p>
-            <div className="control">
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Incrustado en la cabecera
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                En linea
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Hojas de estilo externas
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Mediante Php
-              </label>
-              <br />
-            </div>
-          </section>
-          <section>
-            <p>3.   ¿Que entiendes por css ?</p>
-            <div className="control">
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Class style Sheets
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Creative Style System
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Cascading Style Sheets
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Cascading Style System
-              </label>
-              <br />
-            </div>
-          </section>
-          <section>
-            <p>
-              4.   ¿Qué propiedad utilizamos para definir el color de los bordes?
-            </p>
-            <div className="control">
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Border-color
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Color-border
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Border-bgcolor
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Border-style
-              </label>
-              <br />
-            </div>
-          </section>
-        <section>
-            <p>
-              5.  ¿Con qué propiedad se define el espacio entre el borde de un elemento y los elementos que lo rodean?
-            </p>
-            <div className="control">
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Border
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Margin
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Padding
-              </label>
-              <br />
-              <label className="radio">
-                <input type="radio" name="foobar" />
-                Ninguna respuesta es correcta
-              </label>
-              <br />
-            </div>
-            <div id="button">
-               <button className="button is-primary">Enviar</button>
+          <form>
+            <section className="column">
+              <p>
+                1. ¿Qué propiedad utilizamos para definir una imagen como fondo?
+              </p>
+              <div class="field">
+                <div class="control">
+                  <label class="radio">
+                    <input type="radio" name="question4" />
+                    background-attachment
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question4" />
+                    background-image
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question4" />
+                    background-src
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question4" />
+                    background-img
+                  </label>
+                </div>
               </div>
-          </section>
+
+              <p>
+                2. ¿Cuál es la forma más eficiente de aplicar estilos CSS en un
+                documento HTML?
+              </p>
+              <div class="field">
+                <div class="control">
+                  <label class="radio">
+                    <input type="radio" name="question3" />
+                    Incrustado en la cabecera
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question3" />
+                    En linea
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question3" />
+                    Hojas de estilo externas
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question3" />
+                    Mediante PhP
+                  </label>
+                </div>
+              </div>
+              <p>3. ¿Que entiendes por css ?</p>
+              <div class="field">
+                <div class="control">
+                  <label class="radio">
+                    <input type="radio" name="question1" />
+                    Class style Sheets
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question1" />
+                    Creative Style System
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question1" />
+                    Cascading Style Sheets
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question1" />
+                    Cascading Style System
+                  </label>
+                </div>
+              </div>
+              <p>
+                4. ¿Qué propiedad utilizamos para definir el color de los
+                bordes?
+              </p>
+              <div className="control">
+                <label className="radio">
+                  <input type="radio" name="foobar" />
+                  Border-color
+                </label>
+                <br />
+                <label className="radio">
+                  <input type="radio" name="foobar" />
+                  Color-border
+                </label>
+                <br />
+                <label for="radio" className="radio">
+                  <input type="radio" name="foobar" />
+                  Border-bgcolor
+                </label>
+                <br />
+                <label for="border" className="radio">
+                  <input type="radio" name="foobar" value="border" />
+                  Border-style
+                </label>
+                <br />
+              </div>
+              <p>
+                5. ¿Con qué propiedad se define el espacio entre el borde de un
+                elemento y los elementos que lo rodean?
+              </p>
+              <div class="field">
+                <div class="control">
+                  <label class="radio">
+                    <input type="radio" name="question" />
+                    Border
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question" />
+                    Padding
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question" />
+                    Margin
+                  </label>
+                  <br />
+                  <label class="radio">
+                    <input type="radio" name="question" />
+                    Ninguna respuesta
+                  </label>
+                </div>
+              </div>
+              <div id="button">
+                <button className="button is-primary">Enviar</button>
+              </div>
+            </section>
+          </form>
         </div>
         <Footer />
       </div>
